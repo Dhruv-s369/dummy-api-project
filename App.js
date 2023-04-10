2c28154cc1a2d80b2c6757ea0eae7d9f5e30f191
Dhruv-s369 <126153588+Dhruv-s369@users.noreply.github.com>
2023-04-10 20:10:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ApiCallback from "./components/ApiCallback";
+
+function App() {
+  return (
+    <div className="App">
+      <ApiCallback />
+    </div>
+  );
+}
+
+export default App;
